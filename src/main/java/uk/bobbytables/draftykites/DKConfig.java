3bd913cb31cb6029f077b8baf921cdf9b2b37039
Sam Kirby <sam.kirby94@hotmail.co.uk>
2020-09-05 17:31:02 +0100

    release version
    * Make updraft biome dependent
    
    * Fix length of updraft to avoid client/server desync

--- src/main/java/uk/bobbytables/draftykites/DKConfig.java
+++ src/main/java/uk/bobbytables/draftykites/DKConfig.java
@@ -1,10 +1,18 @@
 package uk.bobbytables.draftykites;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Config(modid = "draftykites")
 @Mod.EventBusSubscriber
@@ -12,11 +20,14 @@ public class DKConfig {
     @Config.Name("Updraft time (ticks)")
     public static int UPDRAFT_TIME = 40;
 
-    @Config.Name("Minimum time between updrafts (ticks)")
-    public static int COOLDOWN_MIN = 200;
+    @Config.Name("Time between updrafts (ticks)")
+    public static int COOLDOWN = 300;
 
-    @Config.Name("Maximum time between updrafts (ticks)")
-    public static int COOLDOWN_MAX = 400;
+    @Config.Name("Drafty Biomes")
+    public static String[] DRAFTY_BIOMES_RAW = new String[]{"minecraft:void"};
+
+    @Config.Ignore
+    public static List<ResourceLocation> DRAFTY_BIOMES = new ArrayList<>();
 
     @SubscribeEvent
     public static void onConfigChangeEvent(ConfigChangedEvent.OnConfigChangedEvent event) {
@@ -24,5 +35,26 @@ public class DKConfig {
             return;
         }
         ConfigManager.sync("draftykites", Config.Type.INSTANCE);
+
+        reloadBiomes();
+    }
+
+    @SubscribeEvent
+    public static void onWorldLoadEvent(WorldEvent.Load event) {
+        reloadBiomes();
+    }
+
+    private static void reloadBiomes() {
+        Logger logger = LogManager.getLogger();
+
+        DRAFTY_BIOMES.clear();
+        for (String possible_resource : DRAFTY_BIOMES_RAW) {
+            ResourceLocation resourceLocation = new ResourceLocation(possible_resource);
+            if (Biome.REGISTRY.containsKey(resourceLocation)) {
+                DRAFTY_BIOMES.add(resourceLocation);
+            } else {
+                logger.error("No biome found for: {}", possible_resource);
+            }
+        }
     }
 }
--- src/main/java/uk/bobbytables/draftykites/entity/EntityVolarkiteExt.java
+++ src/main/java/uk/bobbytables/draftykites/entity/EntityVolarkiteExt.java
@@ -1,7 +1,7 @@
 package uk.bobbytables.draftykites.entity;
 
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.apache.logging.log4j.LogManager;
 import thebetweenlands.common.entity.EntityVolarkite;
 import uk.bobbytables.draftykites.DKConfig;
 
@@ -11,17 +11,19 @@ public class EntityVolarkiteExt extends EntityVolarkite {
 
     public EntityVolarkiteExt(World world) {
         super(world);
-        LogManager.getLogger().info("This is my volakite");
     }
 
     @Override
     protected void updateUpdraft() {
-        if (this.voidTicks <= 0 && this.world.getHeight((int) this.posX, (int) this.posZ) == 0) {
-            this.updraftTicks = DKConfig.UPDRAFT_TIME;
-            this.voidTicks = DKConfig.COOLDOWN_MIN + world.rand.nextInt(DKConfig.COOLDOWN_MAX - DKConfig.COOLDOWN_MIN);
-            LogManager.getLogger().info("Adding updraft");
+        if (this.world.getHeight((int) this.posX, (int) this.posZ) == 0
+                && DKConfig.DRAFTY_BIOMES.contains(this.world.getBiome(new BlockPos(this.posX, this.posY, this.posZ)).getRegistryName())
+        ) {
+            if (this.voidTicks <= 0) {
+                this.updraftTicks = DKConfig.UPDRAFT_TIME;
+                this.voidTicks = DKConfig.COOLDOWN;
+            }
+            this.voidTicks--;
         }
-        this.voidTicks--;
         super.updateUpdraft();
     }
 }
